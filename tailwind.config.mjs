97b5bfeabb211c1800b9a2f9d0c167eb766fcd1d
thonyS07 <thonysilva1993@gmail.com>
2025-01-11 00:49:05 -0500

    Adding header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,14 @@ import animations from 'tailwindcss-animated';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				"custom-white": "0 0 22px #fff8",
+			},
+		
+		},
 	},
 	plugins: [animations],
-}
+};
