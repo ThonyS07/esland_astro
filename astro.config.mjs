0074471b39bded4287e7bda32d6fe022bdfed480
thonyS07 <thonysilva1993@gmail.com>
2025-01-12 23:10:59 -0500

    Adding i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,14 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact()],
+  i18n:{
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    },
+    fallback: {'en': 'es'}
+    
+  }
 });
