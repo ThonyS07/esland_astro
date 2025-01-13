8d75c6575751e50b5d10dc3af1d2320643beb825
thonyS07 <thonysilva1993@gmail.com>
2025-01-12 23:47:43 -0500

    Adding i18n to intro

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,1 +1,13 @@
-export const getI18N=()=> {}
+import english from "./en.json";
+import spanish from "./es.json";
+
+const LANGUAGES = {
+	ENGLISH: "en",
+	SPANISH: "es",
+};
+
+export const getI18N = ({ currentLocale  ='es'}: { currentLocale: string | undefined}) => {
+	if (currentLocale === LANGUAGES.SPANISH) return spanish;
+	if (currentLocale === LANGUAGES.ENGLISH) return english;
+	return spanish;
+};
